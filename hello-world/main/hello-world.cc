fc41adda393a0f51089db691b71c282299db0771
ls-tusimple <lindong.sun@tusimple.ai>
2018-02-08 18:12:51 -0800

    added repo

---
+++ hello-world/main/hello-world.cc
@@ -0,0 +1,21 @@
+#include "lib/hello-greet.h"
+#include "lib/log.h"
+#include <ctime>
+#include <iostream>
+#include <string>
+
+void print_localtime() {
+  std::time_t result = std::time(nullptr);
+  std::cout << std::asctime(std::localtime(&result));
+}
+
+int main(int argc, char** argv) {
+  std::string who = "world";
+  if (argc > 1) {
+    who = argv[1];
+  }
+  std::cout << get_greet(who) << std::endl;
+  print_localtime();
+  AINFO << "From glog!";
+  return 0;
+}
