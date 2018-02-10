9db64da91f5ab555ce8e1eb6eea401388f29d883
ls-tusimple <lindong.sun@tusimple.ai>
2018-02-09 22:39:23 -0800

    try singleton

--- hello-world/main/hello-world.cc
+++ hello-world/main/hello-world.cc
@@ -1,8 +1,38 @@
 #include "lib/hello-greet.h"
-#include "lib/log.h"
 #include <ctime>
 #include <iostream>
 #include <string>
+
+class Singleton
+{
+    private:
+        /* Here will be the instance stored. */
+        static Singleton* instance;
+
+        /* Private constructor to prevent instancing. */
+        Singleton();
+
+    public:
+        /* Static access method. */
+        static Singleton* getInstance();
+};
+
+/* Null, because instance will be initialized on demand. */
+Singleton* Singleton::instance = 0;
+
+Singleton* Singleton::getInstance()
+{
+    if (instance == 0)
+    {
+        instance = new Singleton();
+    }
+
+    return instance;
+}
+
+Singleton::Singleton()
+{}
+
 
 void print_localtime() {
   std::time_t result = std::time(nullptr);
@@ -10,12 +40,12 @@ void print_localtime() {
 }
 
 int main(int argc, char** argv) {
-  std::string who = "world";
-  if (argc > 1) {
-    who = argv[1];
-  }
-  std::cout << get_greet(who) << std::endl;
-  print_localtime();
-  AINFO << "From glog!";
+  //new Singleton(); // Won't work
+  Singleton* s = Singleton::getInstance(); // Ok
+  Singleton* r = Singleton::getInstance();
+
+  /* The addresses will be the same. */
+  std::cout << s << std::endl;
+  std::cout << r << std::endl;
   return 0;
 }
